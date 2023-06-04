d8fa4bbc9781ff919b74ac7da63930b4091a9c89
inhyeok <ss112d86@gmail.com>
2023-06-04 23:19:45 +0900

    chore: project init

---
+++ src/test/java/com/baeminclone/BaemincloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeminclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaemincloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
