8e83fcb93e7061892293fc3fcd42b2b328ef2a3b
이남곤 <nglee.dev@gmail.com>
2023-06-20 14:43:48 +0900

    feat: Customer Entity, SnsProvider

--- src/main/java/com/baeminclone/application/customer/entity/Customer.java
+++ src/main/java/com/baeminclone/application/customer/entity/Customer.java
@@ -1,4 +1,38 @@
 package com.baeminclone.application.customer.entity;
 
-public class Customer {
+import com.baeminclone.application.common.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer extends BaseEntity {
+
+    @Id
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column(name = "customer_id")
+    private String name;
+
+    @Column(name = "customer_email")
+    @Email
+    private String email;
+
+    @NotNull
+    private String snsId;
+
+    @NotNull
+    private SnsProvider snsProvider;
 }
